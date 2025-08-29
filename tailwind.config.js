ed61783a59242ce87eb87104877440ad59d89283
Advaya123 <staradiab@gmail.com>
2025-08-29 18:28:53 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,4 @@
-// // tailwind.config.cjs
-// module.exports = {
-//   content: [
-//     "./index.html",
-//     "./src/**/*.{js,jsx,ts,tsx}", // scan all your React files
-//   ],
-//   theme: {
-//     extend: {
-//       colors: {
-//         botBlue: "#2563eb",   // example custom color
-//         botGray: "#f3f4f6",   // for background bubbles
-//       },
-//     },
-//   },
-//   plugins: [require("@tailwindcss/typography")],
-// };
-
-
-// // tailwind.config.js
+//tailwind.config.js
 export default {
   darkMode: "class",   // enables dark mode with .dark class
   content: [
