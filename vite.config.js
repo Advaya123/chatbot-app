97d9c758e62dcfbf4d428225f6e9c34be8f58710
Advaya123 <staradiab@gmail.com>
2025-08-29 18:29:14 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default {
-//   server: {
-//     proxy: {
-//       "/api": "http://localhost:4000", 
-//     },
-//   },
-// };
-
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   plugins: [react()],
-//   css: {
-//     postcss: './postcss.config.js',
-//   },
-//   server: {
-//     proxy: {
-//       "/api": "http://localhost:4000",
-//     },
-//   },
-// })
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
